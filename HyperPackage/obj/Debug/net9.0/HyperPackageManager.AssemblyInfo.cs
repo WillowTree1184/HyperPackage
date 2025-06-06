02d83f090372324c60329d9e4657d95f290b7bed
WillowTree1184 <xucx_2020@163.com>
2025-06-06 23:27:10 +0800

    Add Basic Stuff

--- HyperPackage/obj/Debug/net9.0/HyperPackageManager.AssemblyInfo.cs
+++ HyperPackage/obj/Debug/net9.0/HyperPackageManager.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("HyperPackageManager")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7d60d7f5e7f2ae160ab3fbfe026672955dcfe9a8")]
 [assembly: System.Reflection.AssemblyProductAttribute("HyperPackageManager")]
 [assembly: System.Reflection.AssemblyTitleAttribute("HyperPackageManager")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
